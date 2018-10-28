b0aca6e319ecd9e188ea073805a96528e297c94c
kanujgit <anujkumar30dec@gmail.com>
2018-10-29 03:27:49 +0530

    291018(1)

---
+++ app/src/main/java/com/sktuligroup/transporttracker/TrackerActivity.java
@@ -0,0 +1,47 @@
+package com.sktuligroup.transporttracker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class TrackerActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracker);
+
+        // Check GPS is enabled
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+
+        // Check location permission is granted - if it is, start
+        // the service, otherwise request the permission
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
+    }
+
+    private void startTrackerService() {
+        startService(new Intent(this, TrackerService.class));
+        finish();
+    }
+}
